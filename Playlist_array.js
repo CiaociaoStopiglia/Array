294754577388ce508551307b4b05634b276b52d4
João Stopiglia <stopiglia.design@gmail.com>
2025-04-25 09:41:18 -0300

    Playlist com array

--- Playlist_array.js
+++ Playlist_array.js
@@ -1,2 +1,9 @@
 // mostre sua playlist favorita
 // crie uma lista com 4 musicas favoritas, e mostre uma por uma com for"
+
+let playlist = ["Vignette", "Subdivisions", "Pedro", "Emergence"];
+let quantidade = playlist.length;
+
+for (let i = 0; i < quantidade; i++) {
+  console.log(playlist[i]);
+}
