32c95f7762a3d564ef17ed761d9227b65dfe57de
João Stopiglia <stopiglia.design@gmail.com>
2025-04-25 09:33:29 -0300

    Time com Array

---
+++ Playlist_array.js
@@ -0,0 +1,2 @@
+// mostre sua playlist favorita
+// crie uma lista com 4 musicas favoritas, e mostre uma por uma com for"
---
+++ Time_array.js
@@ -0,0 +1,5 @@
+//exiba o time dos seus sonhos no console!
+//exiba os 5 nomes pro seu time dos sonhos com Array
+
+let jogadores = ['Lautaro Martinez', "Declan Rice", 'Alexander Arnold', 'Barella', 'Buffon']
+console.log(jogadores);
