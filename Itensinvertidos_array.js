5861e5c28e431be5ffc4b70f1fae1920b8d95b87
João Stopiglia <stopiglia.design@gmail.com>
2025-04-25 10:33:02 -0300

    Itens invertidos com Array

---
+++ Itensinvertidos_array.js
@@ -0,0 +1,10 @@
+//Faça os itens da lista serem exibidos invertidos;
+//Mostre os itens de um array de trás pra frente com um for ao contrário.
+
+let itens = ['Pneu', 'Brinquedo', 'Computador']
+
+let quantida = itens.lenght;
+
+for(let i = 2; i >= 0; i--){
+    console.log(itens[i])
+}
