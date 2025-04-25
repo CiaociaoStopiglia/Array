1bc4acf6a51f4b8e437a0899a05f2e26dc16e9c3
João Stopiglia <stopiglia.design@gmail.com>
2025-04-25 09:49:11 -0300

    Amigos com Array

---
+++ Amigos_array.js
@@ -0,0 +1,7 @@
+//voce criou uma lista com o nome dos seus amigos, mas escreveu o nome de um Deles errado
+//usar um método para corrigir
+
+let amigos = ['Gstuvao', 'Eduardo', 'Clara'];
+amigos[0] = 'Gustavo';
+
+console.log(amigos);
