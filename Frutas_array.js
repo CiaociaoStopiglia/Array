c6a45b1565434057e4f38d27a0e94b4e57f84ab9
João Stopiglia <stopiglia.design@gmail.com>
2025-04-25 11:18:55 -0300

    Total frutas com Array

--- Frutas_array.js
+++ Frutas_array.js
@@ -1,3 +1,6 @@
+//adicionar certa quantidade de frutas e exibir no console quantas tem no total
+//utilizar lenght pra realizar
+
 let frutas = ['abacaxi', 'tomate', 'cereja', 'morango']
 let total = frutas.length
 
