b4a91550e017d3159784bec7f0e711f597ab8c6e
João Stopiglia <stopiglia.design@gmail.com>
2025-04-25 10:27:35 -0300

    Remover ultimo item com Array

---
+++ Removercomida_array.js
@@ -0,0 +1,7 @@
+//crie uma lista e remova o ultimo item da lista
+//utilize "pop()" para remover o ultimo item
+
+let comidas = ['Sushi', 'Temaki', 'Hotroll'];
+comidas.pop()
+
+console.log (comidas)
