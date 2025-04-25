2a7d3d20f0bdd3e1cac8ebd406649e6774a6cdaa
João Stopiglia <stopiglia.design@gmail.com>
2025-04-25 10:52:24 -0300

    Notas com Array

---
+++ Notas_array.js
@@ -0,0 +1,10 @@
+//exiba no console todas as notas dentro de um array
+//utilize "while" para isso
+
+let nota = [9, 10, 2.8, 4.2]
+let i = 0
+
+while (i < nota.length) {
+    console.log("notas: " + nota [i])
+    i++
+}
