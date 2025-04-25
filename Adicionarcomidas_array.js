6d876a7b5f517dca567d24a7339ff391ce8edf7a
João Stopiglia <stopiglia.design@gmail.com>
2025-04-25 09:58:41 -0300

    Lista de comidas com Array

---
+++ Adicionarcomidas_array.js
@@ -0,0 +1,7 @@
+//crie uma lista com 3 comidas, e adicione mais uma no final
+//exiba a ultima usando métodos de array
+
+let comidas = ['Tacos', 'Guacamole', 'Burritos'];
+comidas.push('Tortilla')
+
+console.log(comidas)
